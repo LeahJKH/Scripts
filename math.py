d5f6c1c2d75831aad3211b5e92c0d8c9f5a5b82b
LeahJKH <leahjb06.kodehode@gmail.com>
2024-06-18 14:09:14 +0200

    ez calc

--- math.py
+++ math.py
@@ -1,3 +1,7 @@
+print("this is Leah's calc")
+
+print("")
+
 print("input first number")
 num1 = int(input(""))
 
@@ -7,6 +11,9 @@ op = str(input())
 print("input second number")
 num2 = int(input())
 
+print("")
+
+print("the sum is")
 if op == "+":
     print(num1 + num2)
 if op == "-":
