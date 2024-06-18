9d5bf25faa7b31757f2bbde07a485aac91b885ad
LeahJKH <leahjb06.kodehode@gmail.com>
2024-06-18 12:43:13 +0200

    first working

--- math.py
+++ math.py
@@ -1,1 +1,20 @@
-print("bye world")
+print("input first number")
+num1 = int(input(""))
+
+print("input operator")
+op = str(input())
+
+print("input second number")
+num2 = int(input())
+
+if op == "+":
+    print(num1 + num2)
+if op == "-":
+    print(num1 - num2) 
+if op == "/":
+    print(num1 / num2) 
+if op == "*":
+    print(num1 * num2) 
+if op == "%":
+    print(num1 % num2) 
+
